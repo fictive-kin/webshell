bb36286e5405bbf2caad6d02bebb16cdcaf5146c
scoates <sean@seancoates.com>
2010-08-08 13:34:56 -0400

    clean up and comment

--- wsreadline.js
+++ wsreadline.js
@@ -13,29 +13,27 @@ readline.Interface.prototype.cursorToEnd = function() {
   this.cursor = this.line.length;
 }
 
-var inArray = function(value, array) {
-  for (var i = 0, l = array.length; i < l; i++) {
-    if (array[i] === value) {
-      return true;
-    }
-  }
-  return false;
-};
-
 readline.Interface.prototype.complete = function(chop) {
+  // chop is useful for removing the "complete" character from the input
+  // e.g., if the user presses "tab" then we want to chop off the tab
   if (chop) {
     var line = this.line.substring(0, this.line.length -1);
   } else {
     var line = this.line;
   }
+
   var matches = [];
+
+  // find all matching history items (but avoid duplicates)
   this.history.map(function (val) {
     if (val.substring(0, line.length) == line
       && matches.indexOf(val) == -1) {
       matches.push(val);
     }
   });
+
   if (matches.length > 1) {
+    // more than one match, print matching lines
     sys.puts("\r");
     matches.map(function (val) {
       sys.puts(val + "\r");
@@ -45,11 +43,13 @@ readline.Interface.prototype.complete = function(chop) {
     this.cursorToEnd();
     return false; // did not complete, but matches found
   } else if (matches.length == 1) {
+    // exactly one match, so fill the line
     this.line = matches[0];
     this.prompt();
     this.cursorToEnd();
     return true; // completed
   }
+
   // if we haven't returned yet, that means that there are no matches
   return null;
 }

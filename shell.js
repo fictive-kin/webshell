aed27de8f7b4d49a1a6789c7ba3a5e63cda87171
Evan Haas <evanhaas@gmail.com>
2010-08-24 12:57:26 -0400

    fix broken refactor - was referring to old status variable

--- shell.js
+++ shell.js
@@ -186,7 +186,7 @@ function WebShell(stream) {
       console.log(stylize(msg, 'green'));
     } else if (httpRedirection(code)) {
       console.log(stylize(msg, 'yellow'));
-    } else if (httpClientError(status) || httpServerError(status)) {
+    } else if (httpClientError(code) || httpServerError(code)) {
       console.log(stylize(msg, 'red'));
     }
   };
